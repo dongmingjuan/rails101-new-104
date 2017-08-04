2b2905bb2a34a56c4804841acaa3a5d4d261af34
dongmingjuan <d387918156@gmail.com>
2017-08-04 09:55:37 +0800

    implement user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -7,6 +9,7 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
@@ -14,10 +17,10 @@ class GroupsController < ApplicationController
     end
   end
   def edit
-    @group = Group.find(params[:id])
+
   end
   def update
-    @group = Group.find(params[:id])
+
     if @group.update(group_params)
       redirect_to groups_path
     else
@@ -28,7 +31,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
   def destroy
-    @group = Group.find(params[:id])
+
     @group.destroy
     redirect_to groups_path
   end
@@ -36,6 +39,12 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path
+    end
   end
 
 end
